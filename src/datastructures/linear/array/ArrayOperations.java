62f715a480b06fbe08a8d6e4320963638531ab49
hasiabewardana <hasi.prasanga@gmail.com>
2023-07-19 16:06:49 +0530

    Implemented an insertion algorithm to an array.

--- src/datastructures/linear/array/ArrayOperations.java
+++ src/datastructures/linear/array/ArrayOperations.java
@@ -14,14 +14,21 @@ public class ArrayOperations {
         7. Stop
     * */
     public void insertToArray() {
-        int[] intArray = new int[5];
+        int LA[] = new int[3];
 
-        System.out.println("Array before initialization: " + Arrays.toString(intArray));
+        System.out.println("Array Before Insertion:");
 
-        for (int i = 0; i < intArray.length; i++) {
-            intArray[i] = i + 3;
+        for(int i = 0; i < LA.length; i++)
+            System.out.println("LA[" + i + "] = " + LA[i]); //prints empty array
+
+        System.out.println("Inserting Elements..");
+
+        // Printing Array after Insertion
+        System.out.println("Array After Insertion:");
+
+        for(int i = 0; i < LA.length; i++) {
+            LA[i] = i+3;
+            System.out.println("LA[" + i + "] = " + LA[i]);
         }
-
-        System.out.println("Array after initialization: " + Arrays.toString(intArray));
     }
 }
